b40158c57794c6c33a6f71f217eef44021e1a25b
Rudrala Venkata Aditya <adityarudrala2002@gmail.com>
2026-03-06 17:38:52 +0530

    Parent portal dashboard UI created

--- frontend/Saferide-app/src/App.jsx
+++ frontend/Saferide-app/src/App.jsx
@@ -1,18 +1,21 @@
-import Sidebar from "./components/Sidebar";
-import ParentPortal from "./pages/ParentPortal";
+import Sidebar from "./components/Sidebar"
+
+import ParentPortal from "./pages/ParentPortal"
+
 
 function App() {
   return (
-    <div style={{display:"flex"}}>
+    <div style={{ display: "flex" }}>
 
       <Sidebar />
 
-      <div style={{marginLeft:"20px", width:"100%"}}>
+      <div style={{ width: "100%" }}>
         <ParentPortal />
+
       </div>
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/Saferide-app/src/components/Sidebar.jsx
+++ frontend/Saferide-app/src/components/Sidebar.jsx
@@ -1,66 +1,24 @@
-/*export default function Sidebar(){
-    return(
-        <div style={{
-            width:"200px",
-            height:"100vh",
-            background:"#1e293b",
-            color:"white",
-            padding:"20px"
-        }}>
-            <h2>SafeRide</h2>
-
-            <ul style={{listStyle:"none", padding:0}}>
-                <li>Dashboard</li>
-                <li>Live Tracking</li>
-                <li>Trp history</li>
-                <li>Alerts</li>
-            </ul>
-        </div>
-    )
-}*/
-
 function Sidebar() {
   return (
-    <div
-      style={{
-        width: "220px",
-        height: "100vh",
-        background: "#0f172a",
-        color: "white",
-        padding: "20px",
-      }}
-    >
-      <h2 style={{ marginBottom: "30px" }}>SafeRide</h2>
+    <div style={{
+      width: "220px",
+      height: "100vh",
+      background: "#0f172a",
+      color: "white",
+      padding: "20px"
+    }}>
+      
+      <h2>SafeRide</h2>
 
       <ul style={{ listStyle: "none", padding: 0 }}>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          Dashboard
-        </li>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          Live Tracking
-        </li>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          Trip History
-        </li>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          Alerts
-        </li>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          My Children
-        </li>
-
-        <li style={{ marginBottom: "20px", cursor: "pointer" }}>
-          Settings
-        </li>
-
+        <li style={{ marginBottom: "20px" }}>Dashboard</li>
+        <li style={{ marginBottom: "20px" }}>Live Tracking</li>
+        <li style={{ marginBottom: "20px" }}>Trip History</li>
+        <li style={{ marginBottom: "20px" }}>Alerts</li>
       </ul>
+
     </div>
-  );
+  )
 }
 
 export default Sidebar;
