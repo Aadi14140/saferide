10d677eeb62a8ec0426338c3d6e2ae810f01c89b
Rudrala Venkata Aditya <adityarudrala2002@gmail.com>
2026-03-06 15:42:52 +0530

    Pages Created

---
+++ frontend/Saferide-app/src/pages/ParentPortal.jsx
@@ -0,0 +1,8 @@
+export default function ParentPortal() {
+    return(
+        <div>
+            <h1>SafeRide Parent Portal</h1>
+            <p>Welcome to the parent dashboard</p>
+        </div>
+    )
+}
